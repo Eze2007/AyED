38e04651f26197b1d12405e680ba0031f7fc0463
Ezequiel <ecarnascari@frba.utn.edu.ar>
2026-05-08 02:00:41 -0300

    Add files via upload

---
+++ C++ UTN.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Ingrese su nombre:";
+	string nombre;
+	cin >> nombre;
+	cout << "Ingrese su apellido:";
+	string apellido ;
+		cin >> apellido ;
+	cout << "Hola alumno " << nombre << " " << apellido << endl;
+	return 0;
+}
